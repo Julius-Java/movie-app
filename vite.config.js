d058bbec6eaf8e3f1ae7dada3165863b6ce23228
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-03-15 14:00:38 +0100

    Configure app to make API request via proxy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/api' : {
+      target: "http://www.omdbapi.com",
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    }
+  }
 })
