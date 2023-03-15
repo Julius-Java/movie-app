ca899026cdcaa047d023db889fdc49251794573a
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-03-15 15:22:52 +0100

    Configure vite proxy server for app

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api' : {
-      target: "http://www.omdbapi.com",
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': ''
+  server: {
+    proxy: {
+      '/api' : {
+        target: "http://www.omdbapi.com",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
+
 })
