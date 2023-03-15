3a92fac8d9c5090244589e177cb3ea7ab5279f0d
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-03-15 13:42:06 +0100

    Modify app to make API request through proxy server

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Movie from "./components/Movie"
 const API_KEY = import.meta.env.VITE_MOVIE_API_KEY
 // const API_KEY = process.env.VITE_MOVIE_API_KEY
 
-const API_URL = "http://omdbapi.com?apikey=" + API_KEY
+// const API_URL = "http://omdbapi.com?apikey=" + API_KEY
+const API_URL = `/api/?apikey=${API_KEY}`
 
 function App() {
     const [searchItem, setSearchItem] = useState("")
